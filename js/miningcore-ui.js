7de1d6c67a544704ca6c0fe05f4af6f36e8ef576
MiningCryptoLive <celldr69@gmail.com>
2023-08-08 10:42:09 -0400

    Update miningcore-ui.js

--- js/miningcore-ui.js
+++ js/miningcore-ui.js
@@ -1,5 +1,5 @@
 // config
-var API = 'https://etcsolo.miningcrypto.live/api/'; // API address
+var API = 'https://etc-solo.com/api/'; // API address
 var defaultPool = 'etcsolo'; // Default Pool ID
 
 var currentPool = defaultPool;
